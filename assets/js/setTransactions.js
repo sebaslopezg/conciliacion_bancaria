9760032529f687dcbde1aa8a44c64bb93a92bba8
Sebas <sebaslg96@gmail.com>
2025-09-16 17:46:38 -0500

    parche de lectura de fecha en fecha numerica desde excel

--- assets/js/setTransactions.js
+++ assets/js/setTransactions.js
@@ -31,7 +31,16 @@ function setTransactions(arrMain, arrSecond){
 }
 
 const getTransactionKey = (transaction) => {
-    const dateParts = transaction.date.split('/')
+     let transactionDate
+
+    if (isNaN(transaction.date)) {
+        transactionDate = transaction.date
+    }else{
+        transactionDate = XLSX.SSF.format('d/mm/yyyy', transaction.date)
+    }
+
+    const dateParts = transactionDate.split('/')
+
     const formattedDate = `${dateParts[2]}-${dateParts[1].padStart(2, '0')}-${dateParts[0].padStart(2, '0')}`
     return `${formattedDate}-${transaction.value}`
 }
